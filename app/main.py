2245e35c22989a99eab99600a1cd735b3a5ba16e
Chua Chin Siang <ccs6327@gmail.com>
2018-06-23 15:59:07 +0800

    fix code can’t run

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_url_path='')
 
-@app.routes('/')
+@app.route('/')
+def index():
   return send_from_directory('static', 'index.html')
 
 if __name__ == "__main__":
-    context = ('server.crt', 'server.key')
-    app.run(host='0.0.0.0', ssl_context=context, debug=True)
+  context = ('static/server.crt', 'static/server.key')
+  app.run(host='0.0.0.0', ssl_context=context, debug=True)
