9a0cdec5fb347776641069ced629ec8ba1786b98
chanjunweimy <chanjunweimy@gmail.com>
2018-06-23 17:09:00 +0800

    add audio parsing to flask

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, send_from_directory
+import subprocess
 import session
+import os
 
 app = Flask(__name__, static_url_path='')
 
@@ -10,6 +12,23 @@ def index():
 @app.route('/verifySessionWithUser/<sessionid>/<userid>')
 def verifySessionWithUser(sessionid, userid):
   return str(session.verifySessionHasUser(sessionid, userid))
+
+@app.route('/prescreening/<filename>')
+def prescreening(filename):
+  cleanfile = filename.replace(".wav", "_clean.wav")
+  noisefile = filename.replace(".wav", "_noise.wav")
+  ## If file exists, delete it ##
+  if os.path.isfile(cleanfile):
+    os.remove(cleanfile)
+  ## If file exists, delete it ##
+  if os.path.isfile(noisefile):
+    os.remove(noisefile)  
+  subprocess.Popen("ffmpeg -i " + filename + " -vn -ss 00:00:00 -t 00:00:01 " + noisefile, stdout=subprocess.PIPE).stdout.read()
+  subprocess.Popen("sox " + noisefile + " -n noiseprof noise.prof", stdout=subprocess.PIPE).stdout.read()
+  subprocess.Popen("sox " + filename + " " + cleanfile + " noisered noise.prof 0.21", stdout=subprocess.PIPE).stdout.read()
+  subprocess.Popen("python audioAnalysis.py featureExtractionFile -i " + cleanfile + " -mw 1.0 -ms 1.0 -sw 0.050 -ss 0.050 -o " + cleanfile, stdout=subprocess.PIPE).stdout.read()
+  output = subprocess.Popen("python model.py " + cleanfile + "_st.csv", stdout=subprocess.PIPE).stdout.read()  
+  return output
 
 if __name__ == "__main__":
   context = ('static/server.crt', 'static/server.key')
---
+++ app/model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from sklearn.externals import joblib
+import sys
+import csv
+import numpy as np
+
+MULTI = ["minimal", "mild", "moderate", "moderately_severe", "severe"]
+
+def averageFeatureInFile(csvFilename):
+	if csvFilename.endswith(".csv"):
+		with open(csvFilename, 'rb') as csvfile:
+			spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+			tempFeatureRows = []
+			for row in spamreader:
+				tempFeatureRow = []
+				for cell in row:
+					tempFeatureCellValue = float(cell)
+					tempFeatureRow.append(tempFeatureCellValue)
+				tempFeatureRow = np.array(tempFeatureRow)
+				tempFeatureRows.append(tempFeatureRow)
+			tempFeatureRows = np.array(tempFeatureRows)
+			averageRow = np.mean(tempFeatureRows, axis=0)
+			stdRow = np.std(tempFeatureRows, axis=0)
+			featureRow = np.concatenate([averageRow, stdRow])
+
+			return featureRow
+
+def main():
+	if len(sys.argv) < 2:
+		print "please insert the feature filename"
+		return
+
+	filename = sys.argv[1]
+	X = averageFeatureInFile(filename)
+	
+	cls = joblib.load('model.pkl') 
+	
+	Y = cls.predict(X)
+	print MULTI[Y[0]]
+	
+	
+if __name__ == "__main__": 
+	main()
